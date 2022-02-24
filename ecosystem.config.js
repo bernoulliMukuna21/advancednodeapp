aafc721f2c8fd98dce0837d9d6c3c6df8ebea557
BernoulliMukuna21 <mmbernoulli@gmail.com>
2022-02-24 00:43:09 +0000

    second commit

---
+++ ecosystem.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  apps : [
+    {
+      name: "Advanced NodeJS",
+      script: './bin/www',
+      autorestart: true,
+      watch: true,
+      instances : "max",
+      exec_mode : "cluster",
+      "env_development": {
+        "NODE_ENV"     : "development"
+      },
+      "env_production" : {
+        "NODE_ENV"     : "production",
+      },
+    }
+  ]
+};
+
+/*
+* deploy : {
+    production : {
+      user : 'SSH_USERNAME',
+      host : 'SSH_HOSTMACHINE',
+      ref  : 'origin/master',
+      repo : 'GIT_REPOSITORY',
+      path : 'DESTINATION_PATH',
+      'pre-deploy-local': '',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': ''
+    }
+  }*/
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", (req, res) => {
+  console.log(process.pid)
+  res.send(`${process.pid}`);
+});
+
+router.get("/api/:n", function (req, res) {
+  console.log(process.pid)
+
+  let n = parseInt(req.params.n);
+  let count = 0;
+
+  let startTime = new Date().getTime();
+  if (n > 50000000000) n = 50000000000;
+
+  for (let i = 0; i <= n; i++) {
+    count += i;
+  }
+
+  let endTime = new Date().getTime() - startTime;
+
+  res.send(`Final count is ${count} in ${endTime/1000}seconds and running on ${process.pid}`);
 });
 
 module.exports = router;
